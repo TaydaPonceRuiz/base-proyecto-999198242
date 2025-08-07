cc0759504c90a723a6934a055060b5184709700e
tayda agustin ponce ruiz <taydaponceruiz@gmail.com>
2025-08-07 18:06:51 -0300

    add: contenido estatico en register, preparacion del form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,26 @@ import { Layout } from "../components/Layout"
 const Register = () => {
   return (
     <Layout>
-      <h1>hola desde register</h1>
+      <h1>registrate</h1>
+
+      <section>
+        <h2>hola, bienvenido</h2>
+        <form>
+          <div>
+            <label>username:</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>correo electronico:</label>
+            <input type="email" />
+          </div>
+          <div>
+            <label>contraseña:</label>
+            <input type="password" />
+          </div>
+          <button>ingresar</button>
+        </form>
+      </section>
     </Layout>
   )
 }
