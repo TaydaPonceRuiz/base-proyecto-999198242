9e84c39c32f64b839244902e8779759db01db85b
tayda agustin ponce ruiz <taydaponceruiz@gmail.com>
2025-08-07 18:01:39 -0300

    add: contenido estatico en login, creacion del form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,22 @@ import { Layout } from "../components/Layout"
 const Login = () => {
   return (
     <Layout>
-      <h1>hola desde login</h1>
+      <h1>inicia sesion</h1>
+
+      <section>
+        <h2>hola, bienvenido de nuevo</h2>
+        <form>
+          <div>
+            <label>correo electronico:</label>
+            <input type="email" />
+          </div>
+          <div>
+            <label>contraseña:</label>
+            <input type="password" />
+          </div>
+          <button>ingresar</button>
+        </form>
+      </section>
     </Layout>
   )
 }
