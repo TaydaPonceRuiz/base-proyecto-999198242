c3f7e0ecdec380b735c70e7c49c15268831d8582
tayda agustin ponce ruiz <taydaponceruiz@gmail.com>
2025-08-07 17:52:25 -0300

    add: contenido estatico en dashboard, preparacion de form

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,6 +4,28 @@ const Dashboard = () => {
   return (
     <Layout>
       <h1>hola desde dashboard</h1>
+
+      <section>
+        <h2>Cargar nuevo producto</h2>
+        <form>
+          <div>
+            <label>Nombre del producto:</label>
+            <input type="text" name="nombre" />
+          </div>
+
+          <div>
+            <label>Precio:</label>
+            <input type="number" name="precio" />
+          </div>
+
+          <div>
+            <label>Descripción:</label>
+            <textarea name="descripcion" rows="4" />
+          </div>
+
+          <button>Guardar producto</button>
+        </form>
+      </section>
     </Layout>
   )
 }
