0963a66107be9566579f21f2d7da521389e9ee6a
tayda agustin ponce ruiz <taydaponceruiz@gmail.com>
2025-08-06 21:42:56 -0300

    change: elementos a por Link de react-router-dom en header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom"
+
 const Header = () => {
   return (
-    <header style={{ backgroundColor:"lightblue" }}>
+    <header style={{ backgroundColor:"lightblue"}}>
       <img src="https://upload.wikimedia.org/wikipedia/commons/8/85/Logo-Test.png" alt="imagen de logo" />
       <nav>
         <ul>
           {/*cambiar elementos a por componentes Link de react-router-dom*/}
-          <li><a href="/">inicio</a></li>
-          <li><a href="/dashboard">dashboard</a></li>
-          <li><a href="/login">login</a></li>
-          <li><a href="/registrate">registrate</a></li>
+          <li><Link to="/">inicio</Link></li>
+          <li><Link to="/dashboard">dashboard</Link></li>
+          <li><Link to="/login">login</Link></li>
+          <li><Link to="/registrate">registrate</Link></li>
           <button>cerrar sesion</button>
         </ul>
       </nav>
