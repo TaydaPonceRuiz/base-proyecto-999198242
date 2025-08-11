80fc28dcd37621534207fc47c3f892b226eec2d9
tayda agustin ponce ruiz <taydaponceruiz@gmail.com>
2025-08-11 18:06:41 -0300

    add: rutas privadas y login de usuario

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,27 @@
 import { useEffect, useState } from "react"
 import { Layout } from "../components/Layout"
+import { useAuth } from "../context/UserContext"
 
 const Home = () => {
   const [products, setProducts] = useState([])
-  // simulando existencia del usuario, proximamete este estado sera global
-  const [user, setUser] = useState(true)
+  const [showPopup, setShowPopup] = useState(null)
+  const [productToEdit, setProductToEdit] = useState(null)
+  const [titleEdit, setTitleEdit] = useState("")
+  const [priceEdit, setPriceEdit] = useState("")
+  const [descriptionEdit, setDescriptionEdit] = useState("")
+  const [categoryEdit, setCategoryEdit] = useState("")
+  const [imageEdit, setImageEdit] = useState("")
+
+  // simulando existencia del usuario, proximamente este estado será global
+  const { user } = useAuth()
 
   const fetchingProducts = async () => {
     const response = await fetch("https://fakestoreapi.com/products", { method: "GET" })
     const data = await response.json()
-
     setProducts(data)
   }
 
-  // el array vacio (dependencias) espera a que ejecute el return del jsx. si tiene algo, useEffect se va ejecutar cada vez que se modifique lo que este adentro de la dependencia.
+  // El array vacío (dependencias) espera a que ejecute el return del jsx. Si tiene algo, useEffect se va a ejecutar cada vez que se modifique lo que este dentro de la dependencia.
   useEffect(() => {
     fetchingProducts()
   }, [])
@@ -22,12 +30,58 @@ const Home = () => {
     const response = await fetch(`https://fakestoreapi.com/products/${id}`, { method: "DELETE" })
 
     if (response.ok) {
-      setProducts(prevProducts => prevProducts.filter((product) => product.id != id))
+      setProducts(prevProduct => prevProduct.filter((product) => product.id != id))
       // fetchingProducts()
     }
   }
 
-  // peticion al backend mediante fetch para modificar-> metodo PATCH / PUT http://fakeproductapi.com/products
+  const handleOpenEdit = (product) => {
+    setShowPopup(true)
+    setProductToEdit(product)
+    setTitleEdit(product.title)
+    setPriceEdit(product.price)
+    setDescriptionEdit(product.description)
+    setCategoryEdit(product.category)
+    setImageEdit(product.image)
+  }
+
+  // petición al backend mediante fetch para modificar-> método PATCH / PUT https://fakeproductapi.com/products
+  const handleUpdate = async (e) => {
+    e.preventDefault()
+
+    const updatedProduct = {
+      id: productToEdit.id,
+      title: titleEdit,
+      price: Number(priceEdit),
+      description: descriptionEdit,
+      category: categoryEdit,
+      image: imageEdit
+    }
+
+    try {
+      const response = await fetch(`https://fakestoreapi.com/products/${productToEdit.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updatedProduct)
+      })
+
+      if (response.ok) {
+        const data = await response.json()
+        setProducts(prevProduct =>
+          prevProduct.map((product) =>
+            product.id === productToEdit.id
+              ? data
+              : product
+          ))
+        // fetchingProducts()
+      }
+      setShowPopup(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <Layout>
@@ -58,22 +112,60 @@ const Home = () => {
         <h2>Nuestros productos</h2>
         <p>Elegí entre nuestras categorías más populares.</p>
 
-        {/*Seccion reservada para carga diferida de productos*/}
+
+        {
+          showPopup && <section className="popup-edit">
+            <h2>Editando producto.</h2>
+            <button onClick={() => setShowPopup(null)}>Cerrar</button>
+            <form onSubmit={handleUpdate}>
+              <input
+                type="text"
+                placeholder="Ingrese el titulo"
+                value={titleEdit}
+                onChange={(e) => setTitleEdit(e.target.value)}
+              />
+              <input
+                type="number"
+                placeholder="Ingrese el precio"
+                value={priceEdit}
+                onChange={(e) => setPriceEdit(e.target.value)}
+              />
+              <textarea
+                placeholder="Ingrese la descripción"
+                value={descriptionEdit}
+                onChange={(e) => setDescriptionEdit(e.target.value)}
+              ></textarea>
+              <input
+                type="text"
+                placeholder="Ingrese la categoria"
+                value={categoryEdit}
+                onChange={(e) => setCategoryEdit(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Ingrese la URL de la imagen"
+                value={imageEdit}
+                onChange={(e) => setImageEdit(e.target.value)}
+              />
+              <button>Actualizar</button>
+            </form>
+          </section>
+        }
+
         <div>
           {
             products.map((product) => <div key={product.id}>
               <h2 key={product.id}>{product.title}</h2>
-              <img width="80px" src={product.image} alt={`imagen de ${product.title}`} />
+              <img width="80px" src={product.image} alt={`Imagen de ${product.title}`} />
               <p>${product.price}</p>
               <p>{product.description}</p>
               <p><strong>{product.category}</strong></p>
               {
                 user && <div>
-                  <button>actualizar</button>
-                  <button onClick={() => handleDelete(product.id)}>borrar</button>
-              </div>
+                  <button onClick={() => handleOpenEdit(product)}>Actualizar</button>
+                  <button onClick={() => handleDelete(product.id)}>Borrar</button>
+                </div>
               }
-              
             </div>)
           }
         </div>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,22 @@
 import { Layout } from "../components/Layout"
+import { useAuth } from "../context/UserContext"
 
 const Login = () => {
+
+  const { login } = useAuth()
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    login()
+  }
+
   return (
     <Layout>
       <h1>inicia sesion</h1>
 
       <section>
         <h2>hola, bienvenido de nuevo</h2>
-        <form>
+        <form onSubmit={handleLogin}>
           <div>
             <label>correo electronico:</label>
             <input type="email" />
